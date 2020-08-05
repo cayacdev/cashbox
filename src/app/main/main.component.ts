24456c4aa123f7bb69ab2eb0f94b30adf8363b14
Philipp Müller <pmueller.wf@gmail.com>
2020-08-05 19:20:46 +0200

    close menu when item is selected (only mobile)

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable, Subscription } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { map, shareReplay, take } from 'rxjs/operators';
 import * as fromApp from '../store/app.reducer';
 import { Store } from '@ngrx/store';
 import * as AuthActions from '../auth/store/auth.actions';
@@ -9,27 +9,40 @@ import * as AuthActions from '../auth/store/auth.actions';
 @Component({
   selector: 'app-cash-box',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements OnInit, OnDestroy {
-
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
     .pipe(
-      map(result => result.matches),
+      map((result) => result.matches),
       shareReplay()
     );
 
+  @ViewChild('drawer') drawer: any;
   isAuthenticated = false;
   private authSub: Subscription;
 
-  constructor(private breakpointObserver: BreakpointObserver, private store: Store<fromApp.AppState>) {
-  }
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private store: Store<fromApp.AppState>
+  ) {}
 
   ngOnInit(): void {
-    this.authSub = this.store.select('auth').pipe(
-      map((authState) => authState.user)
-    ).subscribe((user) => {
-      this.isAuthenticated = !!user;
+    this.authSub = this.store
+      .select('auth')
+      .pipe(map((authState) => authState.user))
+      .subscribe((user) => {
+        this.isAuthenticated = !!user;
+      });
+  }
+
+  onClickMenuItem(): void {
+    console.log(this.drawer);
+    this.isHandset$.pipe(take(1)).subscribe((isHandset) => {
+      if (isHandset) {
+        this.drawer.close();
+      }
     });
   }
 
