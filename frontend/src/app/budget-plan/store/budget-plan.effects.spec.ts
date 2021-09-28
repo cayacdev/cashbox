807b2bf4f8c90923ab52c57a93deb7a99a61534a
Philipp Müller <pmueller.wf@gmail.com>
2021-09-28 17:55:53 +0200

    increase frontend test coverage

--- frontend/src/app/budget-plan/store/budget-plan.effects.spec.ts
+++ frontend/src/app/budget-plan/store/budget-plan.effects.spec.ts
@@ -4,9 +4,10 @@ import { Subject } from 'rxjs';
 import { Action } from '@ngrx/store';
 import { provideMockActions } from '@ngrx/effects/testing';
 import * as Test from './budget-plan.actions';
-import { closeBudgetPlan } from './budget-plan.actions';
+import { addBudgetPlan, closeBudgetPlan, updateBudgetPlan } from './budget-plan.actions';
 import { HttpClient } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { BudgetPlan } from '../../model/budget-plan.model';
 
 describe('BudgetPlanEffects', function () {
   let systemUnderTest: BudgetPlanEffects;
@@ -29,50 +30,108 @@ describe('BudgetPlanEffects', function () {
     httpTestingController.verify();
   });
 
-  it('should set budget plan to closed and dispatch to success event', () => {
-    let type = null;
-    systemUnderTest.closeBudgetPlan$.subscribe((action) => {
-      type = action.type;
+  describe('closeBudgetPlan$', () => {
+    it('should set budget plan to closed and dispatch to success event', () => {
+      let type = null;
+      systemUnderTest.closeBudgetPlan$.subscribe((action) => {
+        type = action.type;
+      });
+
+      actions$.next(closeBudgetPlan({ cashBoxId: 1, index: 1, close: true }));
+
+      const req = httpTestingController.expectOne('http://localhost:8080/v1/cash-boxes/1/plans/1/closed');
+      expect(req.request.method).toEqual('PUT');
+      expect(req.request.body).toEqual({ closed: true });
+      req.flush({});
+
+      expect(type).toEqual(Test.updateBudgetPlanSuccess.type);
     });
 
-    actions$.next(closeBudgetPlan({ cashBoxId: 1, index: 1, close: true }));
+    it('should set budget plan to open and dispatch to success event', () => {
+      let type = null;
+      systemUnderTest.closeBudgetPlan$.subscribe((action) => {
+        type = action.type;
+      });
 
-    const req = httpTestingController.expectOne('http://localhost:8080/v1/cash-boxes/1/plans/1/closed');
-    expect(req.request.method).toEqual('PUT');
-    expect(req.request.body).toEqual({ closed: true });
-    req.flush({});
+      actions$.next(closeBudgetPlan({ cashBoxId: 1, index: 1, close: false }));
 
-    expect(type).toEqual(Test.updateBudgetPlanSuccess.type);
+      const req = httpTestingController.expectOne('http://localhost:8080/v1/cash-boxes/1/plans/1/closed');
+      expect(req.request.method).toEqual('PUT');
+      expect(req.request.body).toEqual({ closed: false });
+      req.flush({});
+
+      expect(type).toEqual(Test.updateBudgetPlanSuccess.type);
+    });
+
+    it('should set budget plan to closed and dispatch to failure event if forbidden', () => {
+      let type = null;
+      systemUnderTest.closeBudgetPlan$.subscribe((action) => {
+        type = action.type;
+      });
+
+      actions$.next(closeBudgetPlan({ cashBoxId: 1, index: 1, close: true }));
+
+      const req = httpTestingController.expectOne('http://localhost:8080/v1/cash-boxes/1/plans/1/closed');
+      expect(req.request.method).toEqual('PUT');
+      req.flush({}, { status: 403, statusText: 'some error message' });
+
+      expect(type).toEqual(Test.updateBudgetPlanFail.type);
+    });
   });
 
-  it('should set budget plan to open and dispatch to success event', () => {
-    let type = null;
-    systemUnderTest.closeBudgetPlan$.subscribe((action) => {
-      type = action.type;
+  describe('updateBudgetPlan$', () => {
+    it('should update budget plan and dispatch to success event', () => {
+      let type = null;
+      systemUnderTest.updateBudgetPlan$.subscribe((action) => {
+        type = action.type;
+      });
+
+      const budgetPlan = {
+        id: 1,
+        budget: 200,
+        closed: false,
+        entries: [],
+        name: 'Test',
+        start_date: new Date(),
+        end_date: new Date(),
+      } as BudgetPlan;
+
+      actions$.next(updateBudgetPlan({ cashBoxId: 1, index: 1, budgetPlan }));
+
+      const req = httpTestingController.expectOne('http://localhost:8080/v1/cash-boxes/1/plans/1');
+      expect(req.request.method).toEqual('PUT');
+      expect(req.request.body).toEqual(budgetPlan);
+      req.flush({});
+
+      expect(type).toEqual(Test.updateBudgetPlanSuccess.type);
     });
-
-    actions$.next(closeBudgetPlan({ cashBoxId: 1, index: 1, close: false }));
-
-    const req = httpTestingController.expectOne('http://localhost:8080/v1/cash-boxes/1/plans/1/closed');
-    expect(req.request.method).toEqual('PUT');
-    expect(req.request.body).toEqual({ closed: false });
-    req.flush({});
-
-    expect(type).toEqual(Test.updateBudgetPlanSuccess.type);
   });
 
-  it('should set budget plan to closed and dispatch to failure event if forbidden', () => {
-    let type = null;
-    systemUnderTest.closeBudgetPlan$.subscribe((action) => {
-      type = action.type;
+  describe('addBudgetPlan$', () => {
+    it('should update budget plan and dispatch to success event', () => {
+      let type = null;
+      systemUnderTest.addBudgetPlan$.subscribe((action) => {
+        type = action.type;
+      });
+
+      const budgetPlan = {
+        id: 1,
+        budget: 200,
+        closed: false,
+        entries: [],
+        name: 'Test',
+        start_date: new Date(),
+        end_date: new Date(),
+      } as BudgetPlan;
+
+      actions$.next(addBudgetPlan({ cashBoxId: 1, budgetPlan }));
+
+      const req = httpTestingController.expectOne('http://localhost:8080/v1/cash-boxes/1/plans');
+      expect(req.request.method).toEqual('POST');
+      expect(req.request.body).toEqual(budgetPlan);
+      req.flush({});
+
+      expect(type).toEqual(Test.updateBudgetPlanSuccess.type);
     });
-
-    actions$.next(closeBudgetPlan({ cashBoxId: 1, index: 1, close: true }));
-
-    const req = httpTestingController.expectOne('http://localhost:8080/v1/cash-boxes/1/plans/1/closed');
-    expect(req.request.method).toEqual('PUT');
-    req.flush({}, { status: 403, statusText: 'some error message' });
-
-    expect(type).toEqual(Test.updateBudgetPlanFail.type);
   });
 });
