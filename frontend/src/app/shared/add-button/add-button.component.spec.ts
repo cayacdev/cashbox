042e785970daaf6298c4043e34fade3d37c5e43d
Philipp Müller <pmueller.wf@gmail.com>
2024-07-07 13:32:10 +0200

    deps: update frontend dependencies

--- frontend/src/app/shared/add-button/add-button.component.spec.ts
+++ frontend/src/app/shared/add-button/add-button.component.spec.ts
@@ -1,39 +1,40 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { AddButtonComponent } from './add-button.component';
-import { By } from '@angular/platform-browser';
-import { MatIcon } from '@angular/material/icon';
+import { AddButtonComponent } from './add-button.component'
+import { By } from '@angular/platform-browser'
+import { MatIconTestingModule } from '@angular/material/icon/testing'
 
 describe('AddButtonComponent', () => {
-  let component: AddButtonComponent;
-  let fixture: ComponentFixture<AddButtonComponent>;
+  let component: AddButtonComponent
+  let fixture: ComponentFixture<AddButtonComponent>
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [AddButtonComponent, MatIcon],
-    }).compileComponents();
-  });
+      imports: [MatIconTestingModule],
+      declarations: [AddButtonComponent],
+    }).compileComponents()
+  })
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AddButtonComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(AddButtonComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    expect(component).toBeTruthy()
+  })
 
   it('should emit when button click is received', function () {
-    let isEmitted = false;
+    let isEmitted = false
 
     component.clickReceived.subscribe(() => {
-      isEmitted = true;
-    });
+      isEmitted = true
+    })
 
-    const button: HTMLButtonElement = fixture.debugElement.query(By.css('button')).nativeElement;
-    button.click();
+    const button: HTMLButtonElement = fixture.debugElement.query(By.css('button')).nativeElement
+    button.click()
 
-    expect(isEmitted).toBeTrue();
-  });
-});
+    expect(isEmitted).toBeTrue()
+  })
+})
