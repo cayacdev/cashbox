406bd57880ad75c21e736ff0af2322e843f0098f
Philipp Müller <pmueller.wf@gmail.com>
2024-07-07 12:51:37 +0200

    deps: update frontend dependencies

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,30 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MainComponent } from './main/main.component';
-import { MaterialModule } from './shared/material.module';
-import { AuthModule } from './auth/auth.module';
-import { AuthEffects } from './auth/store/auth.effects';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreModule } from '@ngrx/store';
-import * as fromApp from './store/app.reducer';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { CashBoxModule } from './cash-box/cash-box.module';
-import { CashBoxEffects } from './cash-box/store/cash-boxes/cash-box.effects';
-import { AuthInterceptorService } from './auth/auth-interceptor.service';
-import { environment } from '../environments/environment';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { BudgetPlanEffects } from './budget-plan/store/budget-plan.effects';
-import { UserModule } from './user/user.module';
-import localeDE from '@angular/common/locales/de';
-import { registerLocaleData } from '@angular/common';
-import { NotFoundRoutingModule } from './not-found-routing.module';
-import { ServiceWorkerModule } from '@angular/service-worker';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MainComponent } from './main/main.component'
+import { MaterialModule } from './shared/material.module'
+import { AuthModule } from './auth/auth.module'
+import { AuthEffects } from './auth/store/auth.effects'
+import { EffectsModule } from '@ngrx/effects'
+import { StoreModule } from '@ngrx/store'
+import * as fromApp from './store/app.reducer'
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http'
+import { CashBoxModule } from './cash-box/cash-box.module'
+import { CashBoxEffects } from './cash-box/store/cash-boxes/cash-box.effects'
+import { AuthInterceptorService } from './auth/auth-interceptor.service'
+import { environment } from '../environments/environment'
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'
+import { BudgetPlanEffects } from './budget-plan/store/budget-plan.effects'
+import { UserModule } from './user/user.module'
+import localeDE from '@angular/common/locales/de'
+import { registerLocaleData } from '@angular/common'
+import { NotFoundRoutingModule } from './not-found-routing.module'
+import { ServiceWorkerModule } from '@angular/service-worker'
 
-registerLocaleData(localeDE);
+registerLocaleData(localeDE)
 
 @NgModule({
   declarations: [AppComponent, MainComponent],
@@ -42,6 +42,7 @@ registerLocaleData(localeDE);
     StoreDevtoolsModule.instrument({
       maxAge: 25, // Retains last 25 states
       logOnly: environment.production, // Restrict extension to log-only mode
+      connectInZone: true,
     }),
     NotFoundRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
